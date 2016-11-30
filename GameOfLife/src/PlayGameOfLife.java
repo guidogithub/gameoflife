ad0d91b35d87bf38226d3e040c8aa06cfcefc575
Marty <horset@gmail.com>
2016-11-30 22:43:41 +0100

    set up IntelliJ IDEA project

---
+++ GameOfLife/src/PlayGameOfLife.java
@@ -0,0 +1,9 @@
+/**
+ * Created by horset on 2016/11/30.
+ */
+public class PlayGameOfLife {
+    // Here is the entry point
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
